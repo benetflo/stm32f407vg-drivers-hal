e485a89a2bed6b2c4ec36c14ff3611874874652a
Benji <ben.flodin@outlook.com>
2025-06-14 20:41:47 +0200

    Update stm32f407g_disc1.h

--- drivers/include/stm32f407g_disc1.h
+++ drivers/include/stm32f407g_disc1.h
@@ -7,11 +7,11 @@
  * Base addresses of Flash and SRAM memories
  */
 
-#define FLASH_BASEADDR 				0x08000000U   	//
-#define SRAM1_BASEADDR 				0x20000000U   	//
-#define SRAM2_BASEADDR        0x2001C000U   	//
-#define ROM_BASEADDR          0x1FFF0000U   	//system memory
-#define SRAM 						      SRAM1_BASEADDR	//SRAM1 used as standard
+#define FLASH_BASEADDR           0x08000000U          //
+#define SRAM1_BASEADDR           0x20000000U          //
+#define SRAM2_BASEADDR           0x2001C000U          //
+#define ROM_BASEADDR             0x1FFF0000U          //system memory
+#define SRAM                     SRAM1_BASEADDR       //SRAM1 used as standard
 
 /*
  * AHBx and APBx Bus Peripheral base addresses
