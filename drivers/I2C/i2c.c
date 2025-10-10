8997e01067b152a4992acc2f4b63852de198db14
Benji <ben.flodin@outlook.com>
2025-10-10 17:59:37 +0200

    Update i2c.c

--- drivers/I2C/i2c.c
+++ drivers/I2C/i2c.c
@@ -1,1 +1,127 @@
+#include "stm32f407g-disc1_i2c_driver.h"
+
+
+
+uint16_t AHB_PreScaler[8] = {2,4,8,16,64,128,256,512};
+uint16_t APB1_PreScaler[4] = {2,4,8,16};
+
+void i2c_peri_clk_control(I2C_RegDef_t *pI2Cx, uint8_t EnOrDi){
+	if(EnOrDi == ENABLE){
+		if(pI2Cx == I2C1){
+			I2C1_PCLK_EN();
+		}else if(pI2Cx == I2C2){
+			I2C2_PCLK_EN();
+		}else if(pI2Cx == I2C3){
+			I2C3_PCLK_EN();
+		}
+	}else{
+		if(pI2Cx == I2C1){
+			I2C1_PCLK_DI();
+		}else if(pI2Cx == I2C2){
+			I2C2_PCLK_DI();
+		}else if(pI2Cx == I2C3){
+			I2C3_PCLK_DI();
+		}
+	}
+}
+
+uint32_t RCC_GetPLLOutputClock(){
+	return;
+}
+
+
+uint32_t RCC_GetPCLK1Value(void){
+	uint32_t pclk1, SystemClk;
+
+	uint8_t clksrc, temp, ahbp, apb1p;
+
+	clksrc = ((RCC->CFGR >> 2) & 0x3);
+
+	if(clksrc == 0){
+		SystemClk = 16000000;
+	}else if(clksrc == 1){
+		SystemClk = 8000000;
+	}else if(clksrc == 2){
+		SystemClk = RCC_GetPLLOutputClock();
+	}
+
+	// for AHB
+	temp = ((RCC->CFGR >> 4) & 0xF);
+
+	if(temp < 8){
+		ahbp = 1;
+	}else{
+		ahbp = AHB_PreScaler[temp-8];
+	}
+
+	// for APB1
+	temp = ((RCC->CFGR >> 10) & 0x7);
+
+	if(temp < 4){
+		apb1p = 1;
+	}else{
+		apb1p = APB1_PreScaler[temp-4];
+	}
+
+	pclk1 = (SystemClk / ahbp) / apb1p;
+
+	return pclk1;
+
+
+}
+
+void i2c_init(I2C_Handle_t *pI2CHandle){
+
+	uint32_t tempreg = 0;
+
+	// ACK control bit
+	tempreg |= (pI2CHandle->I2C_Config.I2C_ACKControl << 10);
+
+	// configure the FREQ field of CR2
+	tempreg = 0;
+	tempreg |= RCC_GetPCLK1Value() / 1000000U;
+	pI2CHandle->pI2Cx->CR2 = (tempreg & 0x3F); // mask all the bits except first five bits
+
+	// store the slave address in own address register OAR
+	tempreg |= pI2CHandle->I2C_Config.I2C_DeviceAddress << 1;
+	tempreg |= (1 << 14); // bit 14 needs to be kept at 1 by software
+	pI2CHandle->pI2Cx->OAR1 = tempreg;
+
+	// CCR calculations
+	uint16_t ccr_value = 0;
+	tempreg = 0;
+	if(pI2CHandle->I2C_Config.I2C_SCLSpeed <= I2C_SCL_SPEED_SM){
+		// mode is standard mode. Configure mode in CCR reg 15 bit. Is SM as default
+		ccr_value = (RCC_GetPCLK1Value() / (2 * pI2CHandle->I2C_Config.I2C_SCLSpeed));
+		tempreg |= (ccr_value & 0xFFF);		// only 12 bits are valid, mask out other bits except first 12 bits
+	}else{
+		// mode is fast mode
+		tempreg |= (1 << 15); // fast mode is configured
+		tempreg |= (pI2CHandle->I2C_Config.I2C_FMDutyCycle << 14);
+		if(pI2CHandle->I2C_Config.I2C_FMDutyCycle == I2C_FM_DUTY_2){
+			ccr_value = (RCC_GetPCLK1Value() / (3 * pI2CHandle->I2C_Config.I2C_SCLSpeed));
+		}else{
+			ccr_value = (RCC_GetPCLK1Value() / (25 * pI2CHandle->I2C_Config.I2C_SCLSpeed));
+		}
+		tempreg |= (ccr_value & 0xFFF);		// only 12 bits are valid, mask out other bits except first 12 bits
+	}
+	pI2CHandle->pI2Cx->CCR = tempreg;
+
+	//1. configure the CR1 register and the ACK control bit -> Enables or disables the acking
+	//2. configure the FREQ field of CR2, because I2C uses the FREQ field to decide what is the frequency of APB1
+	//3. Calculate the APB1 clock and store that value in the CR2 register FREQ field
+	//4. Program the device own address into the OAR1 register
+	//5. Calculate the CCR value and store it in the CCR field of the CCR register (important to produce different serial clock speeds)
+	//6. TRISE calculation
+}
+
+void i2c_deinit(I2C_RegDef_t *pI2Cx){
+	if(pI2Cx == I2C1){
+		I2C1_REG_RESET();
+	}else if(pI2Cx == I2C2){
+		I2C2_REG_RESET();
+	}else if(pI2Cx == I2C3){
+		I2C3_REG_RESET();
+	}
+}
 
