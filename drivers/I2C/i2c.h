3a60b6a4ba152dc358fba3594bf9c396b947fd0d
Benji <ben.flodin@outlook.com>
2025-10-15 09:25:36 +0200

    Update i2c.h

--- drivers/I2C/i2c.h
+++ drivers/I2C/i2c.h
@@ -38,11 +38,31 @@ typedef struct
 #define I2C_FM_DUTY_16_9	1
 
 
+// I2C RELATED STATUS FLAGS (masking details)
+#define I2C_FLAG_TXE		(1 << I2C_SR1_TXE)		// Transmit buffer not empty
+#define I2C_FLAG_RXNE		(1 << I2C_SR1_RXNE)		// Receive buffer not empty
+#define I2C_FLAG_SB			(1 << I2C_SR1_SB)
+#define I2C_FLAG_ADDR		(1 << I2C_SR1_ADDR)
+#define I2C_FLAG_BTF		(1 << I2C_SR1_BTF)
+#define I2C_FLAG_STOPF		(1 << I2C_SR1_STOPF)
+#define I2C_FLAG_BERR		(1 << I2C_SR1_BERR)
+#define I2C_FLAG_ARLO		(1 << I2C_SR1_ARLO)
+#define I2C_FLAG_AF			(1 << I2C_SR1_AF)
+#define I2C_FLAG_OVR		(1 << I2C_SR1_OVR)
+#define I2C_FLAG_TIMEOUT	(1 << I2C_SR1_TIMEOUT)
+
 void i2c_peri_clk_control(I2C_RegDef_t *pI2Cx, uint8_t EnOrDi);
 
 void i2c_init(I2C_Handle_t *pI2CHandle);
 void i2c_deinit(I2C_RegDef_t *pI2Cx);
 
+uint8_t I2C_GetFlagStatus(I2C_RegDef_t *pI2Cx, uint32_t FlagName);
+
+/*
+ * Data Send and Receive
+ */
+void i2c_master_send_data(I2C_Handle_t *pI2CHandle, uint8_t *pTxbuffer, uint32_t Len, uint8_t SlaveAddr);
+void i2c_master_recieve_data(I2C_Handle_t *pI2CHandle, uint8_t *pRxbuffer, uint8_t Len, uint8_t SlaveAddr);
 
 void i2c_irq_config(uint8_t irq_num, uint8_t EnOrDi);
 void i2c_irq_prio_config(uint8_t irq_num, uint32_t irq_prio);
@@ -55,4 +75,3 @@ void I2C_ApplicationEventCallback(I2C_Handle_t *pI2CHandle, uint8_t AppEv);
 
 
 #endif /* INC_STM32F407G_DISC1_I2C_DRIVER_H_ */
-
