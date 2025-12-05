c8c9b20d1502461a8e1de533152f7d559897d0f7
Benji <ben.flodin@outlook.com>
2025-12-05 15:40:13 +0100

    Create stm32_uart_tx.c

---
+++ examples/UART/stm32_uart_tx.c
@@ -0,0 +1,98 @@
+#include<stdio.h>
+#include<string.h>
+#include "stm32f407g-disc1.h"
+
+char msg[1024] = "UART Tx testing...\n\r";
+
+USART_Handle_t usart2_handle;
+
+void USART2_Init(void)
+{
+	usart2_handle.pUSARTx = USART2;
+	usart2_handle.USART_Config.USART_Baud = USART_STD_BAUD_115200;
+	usart2_handle.USART_Config.USART_HWFlowControl = USART_HW_FLOW_CTRL_NONE;
+	usart2_handle.USART_Config.USART_Mode = USART_MODE_ONLY_TX;
+	usart2_handle.USART_Config.USART_NoOfStopBits = USART_STOPBITS_1;
+	usart2_handle.USART_Config.USART_WordLength = USART_WORDLEN_8BITS;
+	usart2_handle.USART_Config.USART_ParityControl = USART_PARITY_DISABLE;
+	USART_Init(&usart2_handle);
+}
+
+void USART2_GPIOInit(void)
+{
+	GPIO_Handle_t usart_gpios;
+
+	usart_gpios.pGPIOx = GPIOA;
+	usart_gpios.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_AF;
+	usart_gpios.GPIO_PinConfig.GPIO_PinOPType = GPIO_OUT_TYPE_PP;
+	usart_gpios.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_PIN_PU;   // PULL UP HÄR?
+	usart_gpios.GPIO_PinConfig.GPIO_PinSpeed = GPIO_SPEED_FAST;
+	usart_gpios.GPIO_PinConfig.GPIO_PinAltFunMode =7;
+
+	//USART2 TX
+	usart_gpios.GPIO_PinConfig.GPIO_PinNumber  = GPIO_PIN_NO_2;
+	gpio_init(&usart_gpios);
+
+	//USART2 RX
+	usart_gpios.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_3;
+	gpio_init(&usart_gpios);
+
+
+}
+
+void GPIO_ButtonInit(void)
+{
+	GPIO_Handle_t GPIOBtn,GpioLed;
+
+	//this is btn gpio configuration
+	GPIOBtn.pGPIOx = GPIOA;
+	GPIOBtn.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_0;
+	GPIOBtn.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_IN;
+	GPIOBtn.GPIO_PinConfig.GPIO_PinSpeed = GPIO_SPEED_FAST;
+	GPIOBtn.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_NO_PUPD;
+
+	gpio_init(&GPIOBtn);
+
+	//this is led gpio configuration
+	GpioLed.pGPIOx = GPIOD;
+	GpioLed.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_12;
+	GpioLed.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_OUT;
+	GpioLed.GPIO_PinConfig.GPIO_PinSpeed = GPIO_SPEED_FAST;
+	GpioLed.GPIO_PinConfig.GPIO_PinOPType = GPIO_OUT_TYPE_OD;
+	GpioLed.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_NO_PUPD;
+
+	gpio_peri_clk_control(GPIOD,ENABLE);
+
+	gpio_init(&GpioLed);
+
+}
+
+void delay(void)
+{
+	for(uint32_t i = 0 ; i < 500000/2 ; i ++);
+}
+
+
+int main(void)
+{
+
+	GPIO_ButtonInit();
+
+	USART2_GPIOInit();
+
+    USART2_Init();
+
+    USART_PeripheralControl(USART2,ENABLE);
+
+    while(1)
+    {
+		while( ! gpio_read_pin(GPIOA, 0));
+
+		delay();
+
+    	USART_SendData(&usart2_handle,(uint8_t*)msg,strlen(msg));
+
+    }
+
+	return 0;
+}
